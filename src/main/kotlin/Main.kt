0f9b23e72b9610dc38d19f660921e314330da804
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2023-09-09 16:38:54 -0400

    Reformat codes and format the decimal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,52 @@
+import java.text.DecimalFormat
 import java.util.Scanner
 
 fun main() {
 
     val reader = Scanner(System.`in`)
 
-    val square : Shape = Square("square")
+    // Create a square and ask the user for its sides
+    val square: Shape = Square("square")
     print("Enter the length of the ${square.name}: ")
     val square_len = reader.nextDouble()
     print("Enter the height of the ${square.name}: ")
     val square_hei = reader.nextDouble()
-
     (square as Square).setDimension(square_len, square_hei)
-    displayShapeInfo(square)
 
-    val circle : Shape = Circle("circle")
+    // Create a circle and ask the user for its radius
+    val circle: Shape = Circle("circle")
     print("Enter the radius of the ${circle.name}: ")
     val radius = reader.nextDouble()
-
     (circle as Circle).setDimension(radius)
-    displayShapeInfo(circle)
 
-    val triangle : Shape = Triangle("triangle")
+    // Create a triangle and ask the user for its sides
+    val triangle: Shape = Triangle("triangle")
     print("Enter the 1st side of the ${triangle.name}: ")
     val first_side = reader.nextDouble()
     print("Enter the 2nd side of the ${triangle.name}: ")
     val second_side = reader.nextDouble()
     print("Enter the 3rd side of the ${triangle.name}: ")
     val third_side = reader.nextDouble()
-
     (triangle as Triangle).setDimension(first_side, second_side, third_side)
-    displayShapeInfo(triangle)
 
-    val equilateral_triangle : Shape = EquilateralTriangle("equilateral triangle")
+    // Create a equilateral triangle and ask the user for its side
+    val equilateral_triangle: Shape = EquilateralTriangle("equilateral triangle")
     print("Enter the side of the ${equilateral_triangle.name}: ")
     val side = reader.nextDouble()
-
     (equilateral_triangle as EquilateralTriangle).setDimension(side)
+
+    // Display shapes
+    println("\nHere's the information of all the shapes")
+    displayShapeInfo(square)
+    displayShapeInfo(circle)
+    displayShapeInfo(triangle)
     displayShapeInfo(equilateral_triangle)
 }
 
-fun displayShapeInfo(shape : Shape) {
-    println("==================================\nHere's the information of the ${shape.name}\n==================================")
+fun displayShapeInfo(shape: Shape) {
+    // Create a format decimal to the nearest hundred
+    val dec = DecimalFormat(".00")
+    println("==================================\n${shape.name.capitalize()}")
     shape.printDimensions()
-    println("The area of the ${shape.name} is ${shape.getArea()}\n\n" +
-            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    println("The area of the ${shape.name} is ${dec.format(shape.getArea())}")
 }
